0da2230b09025510109a14605d0d7ac96980d5c3
jmmaltos2 <jmmaltos2@alaska.edu>
2019-10-15 12:36:20 -0800

    added a function for files.hpp

--- ProjectHW2/files.hpp
+++ ProjectHW2/files.hpp
@@ -1,1 +1,24 @@
-#pragma once
+/*
+   files.hpp
+   By: Jared Maltos
+   Created: 10/13/2019
+   CS 202: Homework 02
+   Purpose: .hpp for file
+   Details: Put files.hpp, files.cpp, and filetest.cpp into a project to work
+*/
+
+#ifndef FILES_HPP
+#define FILES_HPP
+
+#include <iostream>
+#include <string>
+using std::string;
+
+#include <ostream>
+using std::ostream;
+
+#include <istream>
+using std::istream;
+
+//function for exercise a
+int getIdFromFile(string filename, istream &in, ostream &out);
