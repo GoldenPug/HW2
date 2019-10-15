0d571097b36d18c9c49080b92542c3d629bc33c3
jmmaltos2 <jmmaltos2@alaska.edu>
2019-10-15 12:41:41 -0800

    added exercise a function to files.cpp

--- ProjectHW2/files.cpp
+++ ProjectHW2/files.cpp
@@ -24,3 +24,54 @@ using std::ofstream;
 
 #include <vector>
 using std::vector;
+
+/*
+Exercise A: this function opens a file with a name which contains up to 1000 unique user names
+			and ID numbers in a text base format
+
+this function getIdFromFile takes 3 parameters (string, istream, ostream) and returns an int
+*/
+int getIdFromFile(string filename, istream &in, ostream &out) {
+
+	ifstream fin(filename.c_str());
+
+	//keeps reading usernames till none are left and returns 0
+	//it repeatedly reads a username from the istream parameter
+	//then it looks up the corresponding ID number in the file
+	if (fin.is_open()) {
+		string str, temp;
+		int tempId, id;
+
+		while (in >> str) {
+			fin.clear();
+			fin.seekg(0, ios::beg);
+			id = -1;
+
+			while (fin >> temp >> tempId) {
+				if (temp == str) {
+					id = tempId;
+					break;
+				}
+			}
+
+			//no username prints error on a line by itself
+			if (id == -1) {
+				out << "error" << endl;
+			}
+
+			//prints id number to ostream on a line by itself
+			else {
+				out << id << endl;
+			}
+
+		}
+
+		return 0;
+	}
+
+	//being unable to open file will return -1
+	else {
+		return -1;
+	}
+
+}
