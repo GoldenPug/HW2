635d0b2b2ca27fa0eb66fef3d6dcb2d02fc9ac61
jmmaltos2 <jmmaltos2@alaska.edu>
2019-10-15 12:40:27 -0800

    created files.cpp

---
+++ ProjectHW2/files.cpp
@@ -0,0 +1,26 @@
+/*
+   files.cpp
+   By:Jared Maltos
+   Created: 10/13/2019
+   CS 202: Homework 02
+   Purpose: .cpp for file
+   Details: Put files.hpp, files.cpp, and filetest.cpp into a project to work
+   Note: include all the .dat files into the same directory as project and files
+*/
+
+#include "files.hpp"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <fstream>
+using std::ios;
+using std::ifstream;
+using std::ofstream;
+
+#include <vector>
+using std::vector;
